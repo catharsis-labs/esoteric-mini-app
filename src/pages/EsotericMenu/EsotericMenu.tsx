20ed6da713dd30081e22d0b9a78da16b6cff150c
Ivan Nesterov <85916405+gogoshbelichi@users.noreply.github.com>
2025-06-21 01:10:59 +0300

    name + avatar access

--- src/pages/EsotericMenu/EsotericMenu.tsx
+++ src/pages/EsotericMenu/EsotericMenu.tsx
@@ -1,15 +1,34 @@
 import { Button } from "@/components/ui/button"
+import {
+    initDataState as _initDataState,
+    initDataRaw as _initDataRaw,
+    useSignal,
+} from '@telegram-apps/sdk-react';
+import {useMemo} from "react";
 
 export function EsotericMenu() {
+    const initDataState = useSignal(_initDataState);
+    const user = useMemo(() => initDataState?.user, [initDataState]);
+    const avatar = user?.photo_url;
+    const firstname = user?.first_name;
+
     return (
         <div className="flex flex-col items-center justify-start p-4 pt-6 space-y-6 min-h-[calc(100vh-5rem)]">
 
             {/* Верхняя панель с балансом и профилем */}
             <div className="w-full flex justify-between items-center px-2">
-                <span className="text-lg font-medium">10$</span>
+                <span className="text-4xl font-medium">10$</span>
                 <div className="flex items-center gap-2">
-                    <span className="text-sm">имя</span>
-                    <div className="w-8 h-8 rounded-full bg-muted" />
+                    <span className="text-4xl flex items-center gap-1">{firstname}</span>
+                    {avatar ? (
+                        <img
+                            src={avatar}
+                            alt="avatar"
+                            className="w-12 h-12 rounded-full object-cover border"
+                        />
+                    ) : (
+                        <div className="w-12 h-12 rounded-full bg-muted" />
+                    )}
                 </div>
             </div>
 
