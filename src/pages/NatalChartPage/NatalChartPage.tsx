cceb9660f06a7bf56e8eb3a6272de70ad4d782ac
Ivan Nesterov <85916405+gogoshbelichi@users.noreply.github.com>
2025-08-16 09:03:05 +0300

    dream interpreter2

--- src/pages/NatalChartPage/NatalChartPage.tsx
+++ src/pages/NatalChartPage/NatalChartPage.tsx
@@ -1,7 +1,5 @@
-import {  } from '@telegram-apps/telegram-ui';
 import type { FC } from 'react';
 
-import {  } from '@/components/Link/Link.tsx';
 import { Page } from '@/components/Page.tsx';
 
 export const NatalChartPage: FC = () => {
