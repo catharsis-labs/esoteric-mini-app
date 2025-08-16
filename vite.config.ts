362a15b2f7f1e1fa928da2075bf96cbe1a46ccd6
Ivan Nesterov <85916405+gogoshbelichi@users.noreply.github.com>
2025-08-16 09:06:46 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
+  base: '/esoteric-mini-app/',
   css: {
     preprocessorOptions: {
       scss: {
